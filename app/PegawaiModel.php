75e457f3904b1d715060b1f70661307c4e672593
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-08-02 01:33:02 +0800

    update

---
+++ app/PegawaiModel.php
@@ -0,0 +1,21 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class PegawaiModel extends Model
+{
+  protected $table = 'tbl_pegawai';
+  public $timestamps = true;
+  protected $fillable = [
+    'nip',
+    'nama',
+    'email',
+    'gd',
+    'gb',
+    'nohp',
+    'image',
+    'status',
+    'kode_unitkerja'
+];
+  
+
+}

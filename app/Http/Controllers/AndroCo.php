37363bd8fc0900993d2405565f3c976c495525e4
Bohati Mulyadi <bohatimulyadi99@gmail.com>
2023-08-06 10:41:05 +0700

    update

--- app/Http/Controllers/AndroCo.php
+++ app/Http/Controllers/AndroCo.php
@@ -635,7 +635,7 @@ public function getdatabyId(Request $r){
       'message'=>'1',
       'data'=>$data,
       'kantor'=>$lokasikantor,
-      'jam'=>($jam != null) ? $jam:$j=['jam'=>'-1:--','batas'=>$data->kode_unitkerja],
+      'jam'=>($jam != null) ? $jam:$j=['jam'=>$dayTerm,'batas'=>'--:--'],
       'bisaabsen'=>$checkabsen,
       'listabsen'=>$absen
     ]);

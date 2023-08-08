20adc5b0d34c14c0c8b5aecbe36477ff80837b1a
Bohati Mulyadi <bohatimulyadi99@gmail.com>
2023-08-08 16:45:31 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -727,15 +727,10 @@ $datamarker = KordinatModel::where('latitude','!=','')
     <!-- Grid column -->
     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
       <!-- Links -->
-      <h6 class="text-uppercase fw-bold">Downlad</h6>
-      <hr
-          class="mb-4 mt-0 d-inline-block mx-auto"
-          style="width: 60px; background-color: #7c4dff; height: 2px"
-          />
-      <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-      <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-      <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-      <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+      <h6 style="color:white" class="text-uppercase fw-bold" style="color:white;">Downlad</h6>
+      <div>
+        <img src="/public/frontend/assets/images/playstoredownload.png" alt="">
+      </div>
     </div>
     <!-- Grid column -->
   </div>
